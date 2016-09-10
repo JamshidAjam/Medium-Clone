97e02601a767a23407dcbf2af5c8a49c55821669
Jamshid <mr.jamshid.a@gmail.com>
2016-09-10 20:04:58 +0430

    improve menu

--- js/script.js
+++ js/script.js
@@ -17,12 +17,44 @@
 
 
 	$(window).bind('scroll', function () {
-	    if ($(window).scrollTop() > 90) {
+	    if ($(window).scrollTop() > 85) {
 	        $('.nav').addClass('fixed');
+	        $('.row').addClass('main-scroll');
 	    } else {
 	        $('.nav').removeClass('fixed');
+	        $('.row').removeClass('main-scroll');
 	    }
 	});
+
+/*
+	$(function () {
+		var sidebar = $('#sidebar');
+		var top = sidebar.offset().top - parseFloat(sidebar.css('margin-top'));
+
+		$(window).scroll(function (event) {
+			var y = $(this).scrollTop();
+			if (y >= top) {
+				sidebar.addClass('fixed-sidebar');
+			} else {
+				sidebar.removeClass('fixed-sidebar');
+			}
+		});
+
+	});
+*/
+
+var linkContainer = $('#sidebar');
+    linkContainerOffset = linkContainer.offset().top;
+    stickyClass = 'sticky-links';
+
+  $(window).on('scroll', function(){
+    var pageOffset = $('body').scrollTop();
+    if (pageOffset > linkContainerOffset-50) {
+      linkContainer.addClass(stickyClass);
+    } else {
+      linkContainer.removeClass(stickyClass);
+    }
+  });
 
 
 });
