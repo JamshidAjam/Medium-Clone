9d7bbcb10197034622e49746f7008ea1aeb9b79e
Jamshid <mr.jamshid.a@gmail.com>
2016-09-13 20:02:51 +0430

    added footer

--- js/script.js
+++ js/script.js
@@ -39,49 +39,11 @@
 	});
 
 /* article */
-// Hide Header on on scroll down
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $('.top-head').outerHeight();
-
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//     }
-// }, 250);
-
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
-    
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
-    
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (st > lastScrollTop && st > navbarHeight){
-//         // Scroll Down
-//         $('.top-head').addClass('.topheadfix');
-// // alert(st + "<br>" + lastScrollTop);
-//     } else {
-//         // Scroll Up
-//         if(st + $(window).height() < $(document).height()) {
-//             $('.top-head').removeClass('.topheadfix');
-//         }
-//     }
-    
-//     lastScrollTop = st;
-// }
-var prev = 0;
-$(window).on('scroll', function(){
-  var scrollTop = $(window).scrollTop();
-  $('.top-head').toggleClass('hide-top-head', scrollTop > prev);
-  prev = scrollTop;
-});
+	var prev = 0;
+	$(window).on('scroll', function(){
+		var scrollTop = $(window).scrollTop();
+		$('.top-head').toggleClass('hide-top-head', scrollTop > prev);
+		prev = scrollTop;
+	});
 
 });
